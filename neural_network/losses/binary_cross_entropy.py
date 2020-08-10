149911fa87e77bf71227d9c070348962c32be263
Korazza <corattiroberto@gmail.com>
2020-08-10 23:01:47 +0200

    Added Binary Cross Entropy

---
+++ neural_network/losses/binary_cross_entropy.py
@@ -0,0 +1,10 @@
+from neural_network.losses import Loss
+import numpy as np
+
+
+class BinaryCrossEntropy(Loss):
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray):
+        return (-1.0 / y_true.size) * np.sum(y_true * np.log(y_pred) + (1.0 - y_true) * np.log(1.0 - y_pred))
+
+    def d(self, y_true: np.ndarray, y_pred: np.ndarray):
+        return -1.0 * ((y_true / y_pred) - ((1.0 - y_true) / (1.0 - y_pred)))

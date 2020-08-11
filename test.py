e3bce35d3fa8e2ad37f448133f8f354d8a7fd27d
Korazza <corattiroberto@gmail.com>
2020-08-11 17:53:23 +0200

    Code refactoring

--- test.py
+++ test.py
@@ -13,6 +13,6 @@ model.add(layers.Dense(3, input_dim=2, activation="sigmoid"))
 model.add(layers.Dense(3, activation="sigmoid"))
 model.add(layers.Dense(1, activation="sigmoid"))
 
-model.train(x, y, learning_rate=0.013, epochs=50000, verbose=2)
+model.train(x, y, learning_rate=0.013, epochs=100000, verbose=2)
 
 model.plot_decision_boundary(x, y)

5e244d8d7aa4c65867a4a0ab5bdc80b000081282
Korazza <corattiroberto@gmail.com>
2020-08-11 17:45:53 +0200

    Code refactoring

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from neural_network import layers, model
-from matplotlib import pyplot as plt
-import numpy as np
 
 # Testing my neural_network module with XOR problem
 
@@ -10,11 +8,11 @@ x = [[0, 0], [0, 1], [1, 0], [1, 1]]
 # labels
 y = [0, 1, 1, 0]
 
-model = model.Model(loss="mean_squared_error")
+model = model.Model(loss="binary_cross_entropy")
 model.add(layers.Dense(3, input_dim=2, activation="sigmoid"))
 model.add(layers.Dense(3, activation="sigmoid"))
 model.add(layers.Dense(1, activation="sigmoid"))
 
-model.train(x, y, learning_rate=0.01, epochs=80000, verbose=2)
+model.train(x, y, learning_rate=0.013, epochs=50000, verbose=2)
 
 model.plot_decision_boundary(x, y)
